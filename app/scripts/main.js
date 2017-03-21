c86c5618175d299bbaed9bbe54b78ac4010b5eb3
Varun G <gvsvarun@gmail.com>
2017-03-21 07:20:19 +0530

    feat: added a base model for chess objects

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,6 +3,16 @@ const Engine = window.engine;
 const Resources = window.resources;
 const Moves = window.moves;
 const Utils = window.utils;
+// Need to have an extensive list of every item on the board. CAn be a static list from another file
+const itemModel = {
+  row: '7',
+  column: 'B',
+  side: 'white',
+  status: 'active',
+  name: 'pawn',
+};
+let currentlySelected = {};
+
 const print = (val) => {
   Utils.log(val);
 };
@@ -19,6 +29,17 @@ const init = () => {
 class Main {
   constructor() {
     this.init = init();
+  }
+  start(item = 'NA') {
+    this.selectedSide = 'white';
+    this.item = item;
+  }
+  select(...args) {
+    currentlySelected = args[0];
+    this.selectedSide = args[1];
+    this.item = args[2];
+    global.currentlySelected = currentlySelected;
+    itemModel.status = 'inactive';
   }
   static init() {
     init();
