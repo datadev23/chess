c88df4f6b34b00f0e13bde85396c914bfea58e09
Varun G <gvsvarun@gmail.com>
2017-03-25 23:41:10 +0530

    feat: Added a canvas object

--- app/scripts/engine.js
+++ app/scripts/engine.js
@@ -1,12 +1,27 @@
 /* global window*/
 const test = () => 'Engine works';
-
+const Utils = window.utils;
+const print = (val) => {
+  Utils.log(val);
+};
 class Engine {
   constructor(engineVal = 'test') {
     this.engine = engineVal;
     this.test = test;
   }
-
+  load() {
+    const doc = global.document;
+    const win = global.window;
+    const canvas = doc.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+    let lastTime;
+    print(ctx, lastTime, win);
+    canvas.width = 505;
+    canvas.height = 606;
+    doc.body.appendChild(canvas);
+    this.loaded = true;
+    // ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);
+  }
   check() {
     return `${this.engine} works`;
   }
