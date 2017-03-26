f556b027557519d3550bbc82837370fd1ead2363
Varun G <gvsvarun@gmail.com>
2017-03-26 23:48:32 +0530

    test: Refactored the test suite

--- test/spec/Engine.test.js
+++ test/spec/Engine.test.js
@@ -2,9 +2,8 @@
 /* eslint no-unused-expressions: "warn"*/
 (function TestSuite() {
   describe('Engine Test Module ', () => {
+    const Engine = window.engine;
     describe('Engine Class', () => {
-      const Engine = window.engine;
-
       it('should be truthy', () => {
         const engine = new Engine();
         expect(engine).not.toBeUndefined();
