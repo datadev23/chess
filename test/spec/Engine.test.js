1fcc9599efa4191ecdcc81c365a95f5e08cbfea7
system dv <system dv>
2017-04-02 18:07:29 +0100

    engine.js test code

--- test/spec/Engine.test.js
+++ test/spec/Engine.test.js
@@ -1,13 +1,39 @@
 /* global window,describe,it,expect*/
 /* eslint no-unused-expressions: "warn"*/
 (function TestSuite() {
-  describe('Engine Test Module ', () => {
-    const Engine = window.engine;
-    describe('Engine Class', () => {
-      it('should be truthy', () => {
-        const engine = new Engine();
-        expect(engine).not.toBeUndefined();
-      });
-    });
-  });
+	 
+
+describe('Engine variable defined', function() {
+
+	it('is the object defined', function() {
+		 const Engine = window.engine;
+		 //console.log(Engine.check());
+		 expect(false).toBeDefined();
+
+	});
+
+	it('check method returns ', function() {
+
+		spyOn(engine, 'check');
+		expect(engine.check).toHaveBeenCalled();
+
+
+		});
+
+		it('load method called ', function() {
+
+		//expect(false).toBeDefined();
+
+        spyOn(engine, 'load');
+		expect(engine.load).toHaveBeenCalled();
+
+		});
+
+
+
+
+
+
+});
+
 }());
