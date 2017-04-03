6bd1a55302291dcd44cca66a7ef77b94130875b8
Varun G <gvsvarun@gmail.com>
2017-04-03 23:01:30 +0530

    test: Updated to the latest spec of Engine

--- test/spec/Engine.test.js
+++ test/spec/Engine.test.js
@@ -1,13 +1,25 @@
-/* global window,describe,it,expect*/
-/* eslint no-unused-expressions: "warn"*/
+/* global window,describe,it,expect,spyOn*/
 (function TestSuite() {
-  describe('Engine Test Module ', () => {
+  describe('Engine variable defined', () => {
     const Engine = window.engine;
-    describe('Engine Class', () => {
-      it('should be truthy', () => {
-        const engine = new Engine();
-        expect(engine).not.toBeUndefined();
-      });
+
+    it('is the object defined', () => {
+      // console.log(Engine.check());
+      const engine = new Engine();
+      expect(engine).toBeDefined();
+    });
+
+    it('check method returns ', () => {
+      const engine = new Engine();
+      spyOn(engine, 'check');
+      expect(engine.check).toHaveBeenCalled();
+    });
+
+    it('load method called ', () => {
+      // expect(false).toBeDefined();
+      const engine = new Engine();
+      spyOn(engine, 'load');
+      expect(engine.load).toHaveBeenCalled();
     });
   });
 }());
