8526630ec6f7625436eafe5196d52da43c358b05
Varun G <gvsvarun@gmail.com>
2017-04-15 12:49:35 +0530

    feat: Update Engine for Performance and consistency

--- app/scripts/engine.js
+++ app/scripts/engine.js
@@ -7,24 +7,28 @@ const print = (val) => {
   Utils.log(val);
 };
 const Resources = window.Resources;
+const Pawn = window.Pawns;
 const resources = new Resources();
 const SpriteImage = window.SpriteImage;
 let spriteImage;
+
 // const columns = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
 const numCols = 8;
 const numRows = 8;
 // const rows = [1, 2, 3, 4, 5, 6, 7, 8];
-const drawPawns = (white, black, ctx, boardDimension) => {
+const drawPawns = (white, black, ctx, boardDimension, allPieces) => {
   const rows = [0, 1, 2, 3, 4, 5, 6, 7];
   const cols = [1, 6];
+  let pawn;
+  let side;
   // const updatedDimensions = boardDimension / 2 - 15;
   cols.forEach((col) => {
     rows.forEach((row) => {
-      if (col === 1) {
-        spriteImage.draw(ctx, 'bPawn', row * boardDimension + 30, col * boardDimension + 5);
-      } else {
-        spriteImage.draw(ctx, 'wPawn', row * boardDimension + 30, col * boardDimension + 5);
-      }
+      // pawn = Pawn.call(this, 'black');
+      side = col === 1 ? 'black' : 'white';
+      pawn = new Pawn(side, row * boardDimension + 30, col * boardDimension + 5, true);
+      spriteImage.draw(ctx, pawn.canvasPosition, pawn.x, pawn.y);
+      allPieces.push(pawn);
     });
   });
 };
@@ -34,7 +38,7 @@ let ctx = {};
 // let blackTile = '';
 class Engine {
   constructor() {
-    this.blackTile = '';
+    document.addEventListener('click', this.boardClick.bind(this));
   }
   load() {
     print('Engine load function called');
@@ -46,17 +50,10 @@ class Engine {
       Utils.alert('Canvas is not supported');
       return;
     }
-    this.allPostions = [];
+
     this.boardDimension = 90;
     this.spriteSheet = 'images/ChessBoardSpriteSheet.png';
-    this.bPawnPos = [36, 210];
-    this.wPawnPos = [35, 311];
-    this.darkTile = [5, 5];
-    this.lightTile = [390, 5];
-    this.allPostions.push({ name: 'darkTile', val: this.darkTile });
-    this.allPostions.push({ name: 'lightTile', val: this.lightTile });
-    this.allPostions.push({ name: 'bPawn', val: this.bPawnPos });
-    this.allPostions.push({ name: 'wPawn', val: this.wPawnPos });
+    this.allPieces = [];
 
     ctx = canvas.getContext('2d');
     resources.loadAll(this.spriteSheet);
@@ -80,33 +77,66 @@ class Engine {
       for (col = 0; col < numCols; col += 1) {
         if (row % 2 === 0) {
           if (col % 2 === 0) {
-            spriteImage.draw(ctx, 'darkTile', col * self.boardDimension, row * self.boardDimension);
-          } else {
-            spriteImage.draw(
-              ctx,
-              'lightTile',
+            ctx.fillStyle = 'white';
+            ctx.fillRect(
               col * self.boardDimension,
               row * self.boardDimension,
+              self.boardDimension,
+              self.boardDimension,
+            );
+          } else {
+            ctx.fillStyle = '#800000';
+            ctx.fillRect(
+              col * self.boardDimension,
+              row * self.boardDimension,
+              self.boardDimension,
+              self.boardDimension,
             );
           }
         } else if (row % 2 !== 0) {
           if (col % 2 === 0) {
-            spriteImage.draw(
-              ctx,
-              'lightTile',
+            ctx.fillStyle = '#800000';
+            ctx.fillRect(
               col * self.boardDimension,
               row * self.boardDimension,
+              self.boardDimension,
+              self.boardDimension,
             );
           } else {
-            spriteImage.draw(ctx, 'darkTile', col * self.boardDimension, row * self.boardDimension);
+            ctx.fillStyle = 'white';
+            ctx.fillRect(
+              col * self.boardDimension,
+              row * self.boardDimension,
+              self.boardDimension,
+              self.boardDimension,
+            );
           }
         }
       }
     }
-    self.drawDefaultPieces(ctx);
+    self.drawDefaultPieces();
+    self.renderAllPieces();
+  }
+  renderAllPieces() {
+    this.allPieces.forEach(piece => piece.render(spriteImage, ctx));
+  }
+  boardClick(evt) {
+    // get cell coordinates
+    const cellX = Math.floor(evt.clientX / this.boardDimension);
+    const cellY = Math.floor(evt.clientY / this.boardDimension);
+
+    const piece = this.allPieces.find(p => p.x === cellX && p.y === cellY);
+
+    if (piece) {
+      // a piece was clicked
+      print(piece);
+    } else {
+      print('Nothing has been CLicked');
+      // an empty cell was clicked
+    }
   }
   drawDefaultPieces() {
-    drawPawns(this.wPawnPos, this.bPawnPos, ctx, this.boardDimension);
+    drawPawns(this.wPawnPos, this.bPawnPos, ctx, this.boardDimension, this.allPieces);
   }
 }
 
